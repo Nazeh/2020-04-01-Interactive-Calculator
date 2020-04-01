73f653170a824bf90bccbdb78ddf15db255a02d4
nazeh <ar.nazeh@gmail.com>
2020-04-01 21:31:03 +0300

    Add Chartjs and basic simulation

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,46 @@
 <script>
-  export let name;
+  import Chart from 'chart.js';
+  import { onMount } from 'svelte';
+
+  let ctx;
+  let growthRate = 1.1;
+  let startWith = 1;
+  const yearsNo = 30;
+  $: years = [startWith];
+  let wealth = [1];
+
+  $: {
+    for (let year = 1; year <= yearsNo; year += 1) {
+      years = [...years, years.slice(-1)[0] + 1];
+      wealth = [...wealth, wealth.slice(-1)[0] * growthRate];
+    }
+  }
+
+  const createChart = (data, xLabels) => {
+    ctx = document.getElementById('myChart').getContext('2d');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: xLabels,
+        datasets: [
+          {
+            label: 'Accumulated',
+            data,
+          },
+        ],
+      },
+    });
+  };
+
+  onMount(() => createChart(wealth, years));
 </script>
 
 <style>
-  h1 {
-    color: purple;
-  }
+
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Compound growth calculator!</h1>
+<canvas id="myChart" width="400" height="400" />
+<input type="range" bind:value={startWith} />
+<input type="range" bind:value={growthRate} />
